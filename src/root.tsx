5cfabfb098165b5eed9a7b74ce01d0cf36b20b94
Luke Gareth Ribchester <luke@luke.gr>
2025-12-18 11:13:17 +0100

    initial commit
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/root.tsx
@@ -0,0 +1,12 @@
+export default () => {
+  return (
+    <>
+      <head>
+        <meta charset="utf-8" />
+        <title>Qwik Blank App</title>
+      </head>
+      <body>
+      </body>
+    </>
+  );
+};
